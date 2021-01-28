f67e8b67a9b903a7329afb4716903a60fe7e92e2
Oscar Vera <overa@servsuite.onmicrosoft.com>
2021-01-27 22:55:07 -0600

    Creacion pantalla lista de specialties

---
+++ app/Http/Controllers/SpecialtyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Specialty;
+
+class SpecialtyController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $specialties = Specialty::all();
+        return view('specialties.index', compact('specialties'));
+    }
+
+    public function create()
+    {
+        return view('specialties.create');
+    }
+
+    public function save(Request $request)
+    {
+       //dd($request->all());
+       $rules = [
+           'name' => 'required|min:3',
+       ];
+       $msssages = [
+        'name.required' => 'Es necesario ingresar un nombre',
+        'name.min' => 'Es necesario ingresar al menos 3 caracteres',
+       ];
+
+       $this->validate($request, $rules, $msssages);
+
+       $specialty = new Specialty();
+       $specialty->name = $request->input('name');
+       $specialty->description = $request->input('description');
+       $specialty->save();
+
+       return redirect('/specialties');
+    }
+
+    public function update(Request $request, Specialty $specialty)
+    {
+       //dd($request->all());
+       $rules = [
+           'name' => 'required|min:3',
+       ];
+       $msssages = [
+        'name.required' => 'Es necesario ingresar un nombre',
+        'name.min' => 'Es necesario ingresar al menos 3 caracteres',
+       ];
+
+       $this->validate($request, $rules, $msssages);
+
+       $specialty->name = $request->input('name');
+       $specialty->description = $request->input('description');
+       $specialty->update();
+
+       return redirect('/specialties');
+    }
+}
---
+++ resources/views/specialties/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.panel')
+
+@section('content')
+
+<div class="card shadow">
+    <div class="card-header border-0">
+        <div class="row align-items-center">
+            <div class="col">
+                <h3 class="mb-0">Especialidades</h3>
+            </div>
+            <div class="col text-right">
+                <a href="{{ url('specialties/create') }}" class="btn btn-sm btn-default">
+                    + Nueva especialidad
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="table-responsive">
+        <!-- Projects table -->
+        <table class="table align-items-center table-flush">
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Descripción</th>
+                    <th scope="col">Opciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                 @foreach ($specialties as $specialty)
+                <tr>
+                    <th scope="row">
+                        {{ $specialty->name }}
+                    </th>
+                    <td>
+                        {{ $specialty->description }}
+                    </td>
+                    <td>
+                        <a href="#" class="btn btn-sm btn-primary">Editar</a>
+                        <a href="#" class="btn btn-sm btn-danger">Eliminar</a>
+                    </td>
+                </tr>                            
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
